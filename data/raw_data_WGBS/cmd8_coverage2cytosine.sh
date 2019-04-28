e381bd7e0783522a111f179b1fecc2d9393eee28
xie186 <xie186@purdue.edu>
2019-04-28 16:42:13 -0400

    Update WGBS info

--- data/raw_data_WGBS/cmd8_coverage2cytosine.sh
+++ data/raw_data_WGBS/cmd8_coverage2cytosine.sh
@@ -1,4 +1,4 @@
 coverage2cytosine --genome_folder ../../data/ref/bismark_index/ -o meth_info_EV1 meth_info_EV1/EV1_bismark_bt2.deduplicated.bismark.cov.gz
 coverage2cytosine --genome_folder ../../data/ref/bismark_index/ -o meth_info_3bstrain1 meth_info_3bstrain1/3bstrain1_bismark_bt2.deduplicated.bismark.cov.gz 
-coverage2cytosine --genome_folder ../../data/ref/bismark_index/ -o meth_info_set1rep1 meth_info_set1rep1/set1rep1_bismark_bt2.deduplicated.bismark.cov.gz
+coverage2cytosine --dir test --genome_folder ../../data/ref/bismark_index/ -o meth_info_set1rep1 meth_info_set1rep1/set1rep1_bismark_bt2.deduplicated.bismark.cov.gz
 
